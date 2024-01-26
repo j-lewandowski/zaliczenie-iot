2951a63d33e7224fdfd8ef63f8a61b0bc1f2d9b1
Jakub Lewandowski <kubalew.pl@gmail.com>
2024-01-26 16:51:58 +0100

    inital commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
