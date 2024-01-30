a3c846f127c4deb4b47ec74f9c85a37cf974d244
Jakub Lewandowski <kubalew.pl@gmail.com>
2024-01-30 14:03:49 +0100

    deployed server

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 const express = require("express");
+const { Server } = require("socket.io");
+const http = require("http");
+
 const app = express();
-require("dotenv").config();
-
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello World!" });
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    allowedHeaders: ["*"],
+  },
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+let currentPrice = {
+  price: 0.0,
+  editedAt: new Date(),
+};
+
+app.use("/", (req, res) => {
+  res.send("Hello from PR1CE server!");
+});
+
+io.on("connection", (socket) => {
+  console.log(`Client-${socket.id} connected!`);
+
+  socket.on("test", () => {
+    const data = {
+      price: 12.5,
+      editedAt: new Date(),
+    };
+    socket.emit("test", data);
+  });
+
+  socket.on("setNewPrice", (data) => {
+    // @TODO - db connection
+    currentPrice = data; // act like DB, gonna implement db connection later on
+
+    socket.emit("priceChanged");
+  });
+
+  socket.on("getCurrentPrice", () => {
+    socket.emit("currentPrice", currentPrice);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`Client-${socket.id} disconnected`);
+  });
+});
+
+const PORT = process.env.PORT || 5500;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
 });
